7ff3a6e6b301d7cf1b68971a63397d8fe73aa937
dwarkesh777 <mrdwarkesh65@gmail.com>
2025-09-24 09:23:01 +0530

    Update 24-09-05.py

--- 24-09-05.py
+++ 24-09-05.py
@@ -11,3 +11,16 @@ greet()
 ## function specification
 - it include function name,parameters(if any) , and return type.
 - ideally ,it mean include docstring
+def add(x,y): # parameter
+    """
+    this function adds two number.
+    
+    parameter :
+    - x(int):the first number
+    - y(int):the second number
+    
+    return
+    int: the sum of two number x and y
+    """
+    result = x+y
+    print(f"sum is:",result) 
